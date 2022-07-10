8419df5b9e40daa0ca5575df4a0b6343c269cd6b
Austin Gae <austingae@gmail.com>
2022-07-09 18:24:32 -0700

    getStaticProps()

--- pages/feed/1.js
+++ pages/feed/1.js
@@ -4,7 +4,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Feed = ({ pageNumber, articles}) => {
+const Feed = ({articles}) => {
 
   return (
     <div>
@@ -22,17 +22,6 @@ const Feed = ({ pageNumber, articles}) => {
           </div>
         );
       })}
-      <p>Page Number: {pageNumber}</p>
-      <button>
-        <Link href={`/feed/${pageNumber-1}`}>
-          <a>Previous Page</a>
-        </Link>
-      </button>
-      <button>
-        <Link href={`/feed/${pageNumber+1}`}>
-          <a>Next Page</a>
-        </Link>
-      </button>
 
 
     </div>
@@ -41,10 +30,8 @@ const Feed = ({ pageNumber, articles}) => {
 
 export default Feed
 
-export async function getServerSideProps(pageContext) {
-  const pageNumber = pageContext.query.slug;
-
-  const apiResponse = await fetch (`https://newsapi.org/v2/everything?q=trump&sortBy=popularity&page=${pageNumber}&pageSize=5&apiKey=c8bca453280c47ca9d79d26ceaf74003`);
+export async function getStaticProps(pageContext) {
+  const apiResponse = await fetch (`https://newsapi.org/v2/everything?q=(coronavirus OR covid-19 or covid or virus)&sortBy=publishedAt&popularity&page=1&pageSize=5&apiKey=c8bca453280c47ca9d79d26ceaf74003`);
   const apiJSON = await apiResponse.json();
 
   const articles = apiJSON.articles;
@@ -52,12 +39,10 @@ export async function getServerSideProps(pageContext) {
 
   return {
     props: {
-      pageNumber: Number.parseInt(pageNumber),
       articles: articles
     }
   }
 }
-
 
 
 
